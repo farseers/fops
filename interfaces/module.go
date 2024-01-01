5e2c2823b909ce644897271a9413b9c50f159a21
steden <1470804@qq.com>
2024-01-01 16:40:38 +0800

    format

--- interfaces/module.go
+++ interfaces/module.go
@@ -1,9 +1,13 @@
 package interfaces
 
 import (
+	"context"
 	"fops/application"
+	"fops/interfaces/job"
 	"github.com/farseer-go/fs/modules"
+	"github.com/farseer-go/tasks"
 	"github.com/farseer-go/webapi"
+	"time"
 )
 
 type Module struct {
@@ -15,5 +19,5 @@ func (module Module) DependsModule() []modules.FarseerModule {
 
 func (module Module) PostInitialize() {
 	// 开启构建
-	//tasks.Run("开启构建", time.Second*1, job.BuildingJob, context.Background())
+	tasks.Run("开启构建", time.Second*1, job.BuildingJob, context.Background())
 }
