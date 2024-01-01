6fddb4ce15c1066666199ed6e7d98c7f395aff10
steden <1470804@qq.com>
2024-01-01 11:38:51 +0800

    修改字段顺序

--- infrastructure/repository/model/logDataPO.go
+++ infrastructure/repository/model/logDataPO.go
@@ -7,12 +7,12 @@ import (
 
 type LogDataPO struct {
 	Id        int64             `gorm:"not null;default:0;comment:主键ID"`
-	CreateAt  dateTime.DateTime `gorm:"type:DateTime64(3);not null;comment:发生时间"`
-	LogLevel  eumLogLevel.Enum  `gorm:"not null;comment:日志等级"`
-	Component string            `gorm:"not null;default:'';comment:组件名称"`
-	Content   string            `gorm:"not null;default:'';comment:日志内容"`
-	TraceId   int64             `gorm:"not null;default:0;comment:上下文ID"`
-	AppId     int64             `gorm:"not null;default:0;comment:应用ID"`
 	AppName   string            `gorm:"not null;default:'';comment:应用名称"`
 	AppIp     string            `gorm:"not null;default:'';comment:应用IP"`
+	AppId     int64             `gorm:"not null;default:0;comment:应用ID"`
+	TraceId   int64             `gorm:"not null;default:0;comment:上下文ID"`
+	Component string            `gorm:"not null;default:'';comment:组件名称"`
+	LogLevel  eumLogLevel.Enum  `gorm:"not null;comment:日志等级"`
+	Content   string            `gorm:"not null;default:'';comment:日志内容"`
+	CreateAt  dateTime.DateTime `gorm:"type:DateTime64(3);not null;comment:发生时间"`
 }
