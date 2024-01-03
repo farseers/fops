c06ac541014f811403b1c6b82cecf4106be5ae5a
steden <1470804@qq.com>
2024-01-03 12:59:29 +0800

    调整索引

--- infrastructure/repository/context/chContext.go
+++ infrastructure/repository/context/chContext.go
@@ -10,15 +10,15 @@ var CHContext *chContext
 
 // EsContext 链路追踪上下文
 type chContext struct {
-	TraceContext        data.TableSet[model.TraceContextPO]        `data:"name=link_trace;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailDatabase data.TableSet[model.TraceDetailDatabasePO] `data:"name=trace_detail_database;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,db_name,table_name,connection_string) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailEs       data.TableSet[model.TraceDetailEsPO]       `data:"name=trace_detail_es;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,index_name,aliases_name) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailEtcd     data.TableSet[model.TraceDetailEtcdPO]     `data:"name=trace_detail_etcd;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,key) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailHand     data.TableSet[model.TraceDetailHandPO]     `data:"name=trace_detail_hand;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,name) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailHttp     data.TableSet[model.TraceDetailHttpPO]     `data:"name=trace_detail_http;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,method,url) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailGrpc     data.TableSet[model.TraceDetailGrpcPO]     `data:"name=trace_detail_grpc;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,method,url) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailMq       data.TableSet[model.TraceDetailMqPO]       `data:"name=trace_detail_mq;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,server,exchange,routing_key) PARTITION BY toYYYYMM(create_at)"`
-	TraceDetailRedis    data.TableSet[model.TraceDetailRedisPO]    `data:"name=trace_detail_redis;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,key,field) PARTITION BY toYYYYMM(create_at)"`
+	TraceContext        data.TableSet[model.TraceContextPO]        `data:"name=link_trace;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailDatabase data.TableSet[model.TraceDetailDatabasePO] `data:"name=trace_detail_database;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,db_name,table_name,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailEs       data.TableSet[model.TraceDetailEsPO]       `data:"name=trace_detail_es;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,index_name,aliases_name,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailEtcd     data.TableSet[model.TraceDetailEtcdPO]     `data:"name=trace_detail_etcd;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,key,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailHand     data.TableSet[model.TraceDetailHandPO]     `data:"name=trace_detail_hand;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,name,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailHttp     data.TableSet[model.TraceDetailHttpPO]     `data:"name=trace_detail_http;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,method,url,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailGrpc     data.TableSet[model.TraceDetailGrpcPO]     `data:"name=trace_detail_grpc;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,method,url,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailMq       data.TableSet[model.TraceDetailMqPO]       `data:"name=trace_detail_mq;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,server,exchange,routing_key,start_ts) PARTITION BY toYYYYMM(create_at)"`
+	TraceDetailRedis    data.TableSet[model.TraceDetailRedisPO]    `data:"name=trace_detail_redis;migrate=ReplacingMergeTree() ORDER BY (app_name,parent_app_name,app_ip,app_id,trace_id,key,field,start_ts) PARTITION BY toYYYYMM(create_at)"`
 	LogData             data.TableSet[model.LogDataPO]             `data:"name=log_data;migrate=ReplacingMergeTree() ORDER BY (app_name,component,log_level,app_ip,app_id,trace_id,create_at,log_id) PARTITION BY toYYYYMM(create_at)"`
 }
 
