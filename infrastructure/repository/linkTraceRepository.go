880a013a0065c14c1d6731d36755ef5b456ac5e6
steden <1470804@qq.com>
2024-01-03 14:36:15 +0800

    添加select 字段

--- infrastructure/repository/linkTraceRepository.go
+++ infrastructure/repository/linkTraceRepository.go
@@ -73,7 +73,7 @@ func (receiver *linkTraceRepository) ToEntity(traceId int64) collections.List[li
 
 func (receiver *linkTraceRepository) ToWebApiList(appName, appIp, requestIp, searchUrl string, statusCode int, searchUseTs int64, startMin int, pageSize, pageIndex int) collections.PageList[linkTraceCom.TraceContext] {
 	if linkTrace.Config.Driver == "clickhouse" {
-		ts := context.CHContext.TraceContext.Select("trace_id,app_id,app_name,app_ip,parent_app_name,trace_type,start_ts,end_ts,use_ts,use_desc,create_at,web_domain,web_path,web_method,web_content_type,web_status_code,web_request_ip").
+		ts := context.CHContext.TraceContext.Select("trace_id,app_id,app_name,app_ip,parent_app_name,trace_type,start_ts,end_ts,use_ts,use_desc,create_at,web_domain,web_path,web_method,web_content_type,web_status_code,web_request_ip,web_request_body,web_response_body,web_headers").
 			Where("trace_type = ? and parent_app_name = ''", eumTraceType.WebApi).
 			WhereIf(appName != "", "app_name = ?", appName).
 			WhereIf(appIp != "", "app_ip = ?", appIp).
