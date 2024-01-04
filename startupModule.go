09fc94c17dd70f8632e3014af6b0d917e2fd006e
steden <1470804@qq.com>
2024-01-04 14:40:59 +0800

    添加设置Git代理

--- startupModule.go
+++ startupModule.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fops/infrastructure"
 	"fops/interfaces"
+	"github.com/farseer-go/fs/configure"
+	"github.com/farseer-go/fs/flog"
 	"github.com/farseer-go/fs/modules"
+	"github.com/farseer-go/utils/exec"
 )
 
 type StartupModule struct {
@@ -20,6 +23,16 @@ func (module StartupModule) Initialize() {
 }
 
 func (module StartupModule) PostInitialize() {
+	// 使用git代理
+	receiveOutput := make(chan string, 100)
+	if gitAgent := configure.GetString("Fops.GitAgent"); gitAgent != "" {
+		flog.Info("开启Git代理：", gitAgent)
+		exec.RunShell("git config --global http.https://github.com.proxy "+gitAgent, receiveOutput, nil, "")
+		exec.RunShell("git config --global https.https://github.com.proxy "+gitAgent, receiveOutput, nil, "")
+	} else {
+		exec.RunShell("git config --global --unset http.https://github.com.proxy", receiveOutput, nil, "")
+		exec.RunShell("git config --global --unset https.https://github.com.proxy", receiveOutput, nil, "")
+	}
 }
 
 func (module StartupModule) Shutdown() {
