0f9a3419615c69ba977ffc86e57006bb6123a163
steden <1470804@qq.com>
2024-01-05 22:30:22 +0800

    修复缺少DockerfilePath字段的赋值

--- application/appsApp/request/addRequest.go
+++ application/appsApp/request/addRequest.go
@@ -4,9 +4,11 @@ import "github.com/farseer-go/collections"
 
 type AddRequest struct {
 	AppName           string                  `validate:"required" label:"应用名称"` // 应用名称
-	ShellScript       string                  // Shell脚本
 	AppGit            int64                   // 应用的源代码
 	FrameworkGits     collections.List[int64] // 依赖的框架源代码
+	ShellScript       string                  // Shell脚本
+	Dockerfile        string                  // Dockerfile内容
+	DockerfilePath    string                  // DockerfilePath路径
 	HealthInstance    int                     // 健康的实例数量
 	DockerReplicas    int                     // 副本数量
 	DockerNodeRole    string                  // 容器节点角色 manager or worker
